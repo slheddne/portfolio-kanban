03df2eb8e9c0fa1c8df04a6b8578bd80f203292c
Salah-Eddine <31400rng@gmail.com>
2024-07-17 23:04:45 +0200

    [fixed] : footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Linkedin } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-300/50 dark:bg-slate-800 p-2 flex flex-col items-center justify-center w-full fixed bottom-0 h-fit'>
+    <footer className='bg-slate-300/50 dark:bg-slate-800 p-2 flex flex-col items-center justify-center w-full h-fit md:fixed md:bottom-0'>
       <p className='text-xs text-center mb-2'>
         Made with ❤️ by Salah-Eddine ET-TALEBY © 2024 - All rights reserved.
       </p>

d31f7f590e4fb3e12a136cb6187b33f481e145c5
unknown <s.ettaleby@epsi.fr>
2025-10-19 21:45:26 +0200

    [added] : refactoring (cards) + age calculation

---
+++ utils/utilFunctions.ts
@@ -0,0 +1,9 @@
+export function calculateAge(birthDate: Date): number {
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
